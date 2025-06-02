0031258f1ea39866e88d4bee9e209773bf349a4b
Eduardo Henrique <eduardohgc3@gmail.com>
2025-06-02 08:46:41 -0300

    Script de direcionamento para credenciais

---
+++ paginas/cadastro/script.js
@@ -0,0 +1,11 @@
+let nome = "";
+let sobrenome = "";
+let data = "";
+document.getElementById("form-cadastro").addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    nome = document.getElementById('input-nome').value;
+    sobrenome = document.getElementById('input-sobrenome').value;
+    data = document.getElementById('input-data').value;
+    console.log(nome, sobrenome, data);
+    window.location.href = "./credenciais.html";
+})
