430c6eac619be654ad8de72af47a6b4ba7c3e2b2
Eduardo Henrique <eduardohgc3@gmail.com>
2025-06-05 19:32:17 -0300

    Cadastro de estamelecimento com fatura e meta de consumo

--- paginas/home/scriptcada.js
+++ paginas/home/scriptcada.js
@@ -1,7 +1,7 @@
 const APP_ID = 'ubZ4XLWmNivxZCMH7ArJ4ck8bwkf67OEt9VOGNHF';
 const API_KEY = 'ZhfsOKyedOFj6E4RDYpgasmvvjPEmoDICFOlBB1R';
 
-document.addEventListener("DOMContentLoaded", (event) => {
+/*document.addEventListener("DOMContentLoaded", (event) => {
     event.preventDefault();
     
     requisicao().then((dados) => {
@@ -108,7 +108,9 @@ function criarElementos(resultado){
 
         estabelecimento.appendChild(card);
     });
-}
+}*/
+
+
 
 document.getElementById("novo-estabelecimento").addEventListener("submit", async (evento) => {
     evento.preventDefault();
@@ -118,6 +120,9 @@ document.getElementById("novo-estabelecimento").addEventListener("submit", async
     const cep = document.getElementById("input-cep").value;
     const tipo = document.getElementById("input-tipo").value;
     const endereco = document.getElementById("input-endereco").value;
+    const metaConsumo = document.getElementById("input-meta-consumo").value;
+    const valorFatura = document.getElementById("input-conta").value;
+    const vencimentoFatura = document.getElementById("input-vencimento-fatura").value;
 
     const dados = {
         id_usuario: {
@@ -137,11 +142,46 @@ document.getElementById("novo-estabelecimento").addEventListener("submit", async
         const resposta = await cadastrarEstabelecimento(dados);
         alert("Imóvel cadastrado com sucesso");
         console.log(resposta);
-        window.location.href = "./home.html";
+        const id_estabelecimento = resposta.objectId;
+        const data = {
+            "__type": "Date",
+            "iso": new Date(resposta.createdAt).toISOString()
+        };
+       // window.location.href = "./home.html";
+        
+       const consumo = parseInt(metaConsumo);
+        const dadosMetaConsumo = {
+        id_estabelecimento: {
+            __type: "Pointer",
+            className: "Estabelecimentos",
+            objectId: id_estabelecimento
+            },
+            consumo: consumo,
+            data: data
+        }  
+        const respostaCosumo = await cadastrarMetaCosumo(dadosMetaConsumo);
+        console.log(respostaCosumo);
+    
+        const fatura = parseFloat(valorFatura);
+        const vencimento = {
+            "__type": "Date",
+            "iso": new Date(vencimentoFatura).toISOString()
+        };
+        const dadosFatura = {
+            id_estabelecimento: {
+                __type: "Pointer",
+                className: "Estabelecimentos",
+                objectId: id_estabelecimento
+            },
+            data_vencimento: vencimento,
+            valor: fatura
+        }
+        const respostaFatura = await cadastrarFatura(dadosFatura);
+        console.log(respostaFatura);
     }catch(erro){
         console.error("Erro ao cadastrar:", erro);            
         alert(erro.message);
-    }
+    } 
 })
 
 async function cadastrarEstabelecimento(dados){
@@ -157,7 +197,47 @@ async function cadastrarEstabelecimento(dados){
     })
 
     if(!resposta.ok){
-        throw new Error(`Erro HTTP: ${resposta.status}`);
+        throw new Error(`Erro HTTP: ${resposta.status} no cadastro do estabelecimento`);
+    }
+
+    const dado = await resposta.json();
+
+    return dado;
+}
+
+async function cadastrarMetaCosumo(dados) {
+    const resposta = await fetch("https://parseapi.back4app.com/classes/meta_consumo/", {
+        method: 'POST',
+        headers: {
+            'X-Parse-Application-Id': APP_ID,
+            'X-Parse-REST-API-Key': API_KEY,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    })
+
+    if(!resposta.ok){
+        throw new Error(`Erro HTTP: ${resposta.status} - no cadastro da meta de cosumo`);
+    }
+
+    const dado = await resposta.json();
+
+    return dado;
+}
+
+async function cadastrarFatura(dados) {
+    const resposta = await fetch("https://parseapi.back4app.com/classes/fatura/", {
+        method: 'POST',
+        headers: {
+            'X-Parse-Application-Id': APP_ID,
+            'X-Parse-REST-API-Key': API_KEY,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    })
+
+    if(!resposta.ok){
+        throw new Error(`Erro HTTP: ${resposta.status} - no cadastro da fatura`);
     }
 
     const dado = await resposta.json();
